f11b9a586c5de8b554a8387b1eacdf493b462dca
katakamrupendra <katakamrupendra@gmail.com>
2026-04-02 12:08:50 +0530

    refactor: replace node array with labeled dictionary and update visualization to a scatter plot with coordinate labels

--- node.py
+++ node.py
@@ -5,15 +5,29 @@ import numpy as np
 
 
 # nodes are at (10,10), (20,20), (30,30), (40,40), (50,50)
-nodes = np.array([[10,10], [20,20], [30,30], [40,40], [50,50]])
+nodes = {
+    'A': (10,10),
+    'B': (20,20),
+    'C': (30,30),
+    'D': (40,40),
+    'E': (50,50)
+}
 
-#create a map of 100x100
-grid = np.zeros((100,100))
-plt.figure()
-plt.imshow(grid)
-plt.xticks(np.arange(0,101,10))
-plt.yticks(np.arange(0,101,10))
-plt.grid(True)
-plt.title("100x100 Grid")
+x_coords = [coord[0] for coord in nodes.values()]
+y_coords = [coord[1] for coord in nodes.values()]
+labels = list(nodes.keys())
+
+plt.figure(figsize=(8, 8))
+plt.scatter(x_coords, y_coords, color='red', s=100) # Plot nodes
+
+for label, x, y in zip(labels, x_coords, y_coords):
+    plt.text(x + 2, y + 2, label, fontsize=12, fontweight='bold')
+
+plt.xlim(0, 100)
+plt.ylim(0, 100)
+plt.grid(True, linestyle='--', alpha=0.6)
+plt.title("5-Node Graph Map (100x100)")
+plt.xlabel("X-axis (Distance from Reference)")
+plt.ylabel("Y-axis (Distance from Reference)")
+
 plt.show()
-
